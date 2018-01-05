2983eb0e38b89a290105fa7e6c31cc90a25e4765
chris <rednianred@gmail.com>
2018-01-05 16:36:07 +0800

    Initial Commit

---
+++ resources/views/layouts/admin/footer.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RedZ
+ * Date: 1/5/2018
+ * Time: 2:38 PM
+ */
---
+++ resources/views/layouts/admin/nav.blade.php
@@ -0,0 +1,14 @@
+<div class="sub-nav mb-24">
+    <div class="container">
+        <ul class="pull-left">
+            <li><a href="alumnus.html">Alumnus</a></li>
+            <li><a href="company_visitor.html">Company</a></li>
+            <li><a href="advertising_register.html">Advertising</a></li>
+            <li><a class="active" href="account_sysad.html">Account</a></li>
+            <li><a href="reports_tesda.html">Reports</a></li>
+            <li><a href="">Announcements</a></li>
+            <li><a href="">Invitation</a></li>
+        </ul>
+
+    </div>
+</div>
