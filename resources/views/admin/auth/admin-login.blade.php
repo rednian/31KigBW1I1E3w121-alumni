a64dea112eb87aa31d2a7e8a46f604a77143e883
chris <rednianred@gmail.com>
2018-01-25 17:52:15 +0800

    finish login admin

--- resources/views/admin/auth/admin-login.blade.php
+++ resources/views/admin/auth/admin-login.blade.php
@@ -127,12 +127,13 @@
 
 <div class="container">
 
-    <form class="form-signin">
+    <form class="form-signin" method="post" action="{{route('admin.login.submit')}}">
+        {{csrf_field()}}
         <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputEmail" class="sr-only">Username</label>
+        <input type="text" name="username" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
 
         <div class="checkbox">
             <label>
