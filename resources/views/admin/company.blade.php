af4a85e3827e3a1541679a034329a3aee73a5da2
chris <rednianred@gmail.com>
2018-03-07 13:53:24 +0800

    update

--- resources/views/admin/company.blade.php
+++ resources/views/admin/company.blade.php
@@ -63,9 +63,7 @@
 
                                             </div>
                                             <div class="modal-footer">
-                                                <button type="button" class="btn btn-default" data-dismiss="modal">
-                                                    Close
-                                                </button>
+                                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                                                 <button type="submit" class="btn btn-success btn-prime box-edge">Register Company</button>
                                             </div>
                                             </form>
